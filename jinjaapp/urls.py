ddf8bd1dea127ef1acba5fdddb30e042479ae4ce
n-muthuri <nelnkatha93@gmail>
2024-11-09 12:15:50 +0300

    Initial Commit

---
+++ jinjaapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .import views
+
+
+
+urlpatterns=[
+    path('',views.home,name='my_home'),
+    path('about/',views.about,name='my_about'),
+    path('contact/',views.contact,name='my_contact'),
+    path('services/',views.services,name='my_services')
+
+
+]
